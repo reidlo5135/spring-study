df31016cab6652f9cc519151ffcea9d590e27997
reidlo5135 <201840103@email.daelim.ac.kr>
2022-01-08 21:46:16 +0900

    ex05 1/8 Commit

--- ex05/src/main/java/org/zerock/controller/BoardController.java
+++ ex05/src/main/java/org/zerock/controller/BoardController.java
@@ -1,5 +1,8 @@
 package org.zerock.controller;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -112,17 +115,46 @@ public class BoardController {
 	}
 	
 	@PostMapping("/remove")
-	public String remove(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, RedirectAttributes rttr) {
+	public String remove(@RequestParam("bno") Long bno, Criteria cri, RedirectAttributes rttr) {
 		log.info("remove......" + bno);
+		
+		List<BoardAttachVO> attachList = service.getAttachList(bno);
+		
 		if(service.remove(bno)) {
+			deleteFiles(attachList);
 			rttr.addFlashAttribute("result", "success");
 		}
 		
-		rttr.addAttribute("pageNum", cri.getPageNum());
-		rttr.addAttribute("amount", cri.getAmount());
-		rttr.addAttribute("type", cri.getType());
-		rttr.addAttribute("keyword", cri.getKeyword());
+//		rttr.addAttribute("pageNum", cri.getPageNum());
+//		rttr.addAttribute("amount", cri.getAmount());
+//		rttr.addAttribute("type", cri.getType());
+//		rttr.addAttribute("keyword", cri.getKeyword());
 		
-		return "redirect:/board/list";
+		return "redirect:/board/list" + cri.getListLink();
+	}
+	
+	private void deleteFiles(List<BoardAttachVO> attachList) {
+		if(attachList == null || attachList.size() == 0) {
+			return;
+		}
+		
+		log.info("delete attach files.............");
+		log.info(attachList);
+		
+		attachList.forEach(attach -> {
+			try {
+				Path file = Paths.get("C:\\upload\\" + attach.getUploadPath() + "\\" + attach.getUuid() + "_" + attach.getFileName());
+				
+				Files.deleteIfExists(file);
+				
+				if(Files.probeContentType(file).startsWith("image")) {
+					Path thumbNail = Paths.get("C:\\upload\\" + attach.getUploadPath() + "\\s" + attach.getUuid() + "_" + attach.getFileName());
+					
+					Files.delete(thumbNail);
+				}
+			} catch (Exception e) {
+				log.error("delete file error" + e.getMessage());
+			}
+		});
 	}
 }
--- ex05/src/main/java/org/zerock/mapper/BoardAttachMapper.java
+++ ex05/src/main/java/org/zerock/mapper/BoardAttachMapper.java
@@ -7,6 +7,10 @@ import org.zerock.domain.BoardAttachVO;
 public interface BoardAttachMapper {
 	
 	public void insert(BoardAttachVO vo);
+	
 	public void delete(String uuid);
+	
 	public List<BoardAttachVO> findByBno(Long bno);
+	
+	public void deleteAll(Long bno);
 }
