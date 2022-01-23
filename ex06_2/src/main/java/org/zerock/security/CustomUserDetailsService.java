08d68d249d9261d9b4e5f060010cabbf7d33f589
reidlo5135 <201840103@email.daelim.ac.kr>
2022-01-23 12:55:10 +0900

    ex06_2 Settings Commit

--- ex06_2/src/main/java/org/zerock/security/CustomUserDetailsService.java
+++ ex06_2/src/main/java/org/zerock/security/CustomUserDetailsService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.zerock.domain.CustomUser;
+import org.zerock.security.domain.CustomUser;
 import org.zerock.domain.MemberVO;
 import org.zerock.mapper.MemberMapper;
 
--- ex06_2/src/main/java/org/zerock/security/domain/CustomUser.java
+++ ex06_2/src/main/java/org/zerock/security/domain/CustomUser.java
@@ -1,4 +1,4 @@
-package org.zerock.domain;
+package org.zerock.security.domain;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.zerock.domain.MemberVO;
 
 import lombok.Getter;
 
