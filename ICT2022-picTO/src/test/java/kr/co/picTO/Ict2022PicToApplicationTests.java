cde7a93e08a383fb2aa99e01fc78a633314eae80
reidlo5135 <201840103@email.daelim.ac.kr>
2022-03-08 17:48:31 +0900

    3/8 setting
    Tomcat Settings

--- ICT2022-picTO/src/test/java/kr/co/picTO/Ict2022PicToApplicationTests.java
+++ ICT2022-picTO/src/test/java/kr/co/picTO/Ict2022PicToApplicationTests.java
@@ -3,11 +3,15 @@ package kr.co.picTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootTest
+@Log4j2
 class Ict2022PicToApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Boot Test");
 	}
 
 }
