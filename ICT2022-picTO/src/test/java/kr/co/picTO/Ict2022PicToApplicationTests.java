2bd07b8381ae3a9c8b66d32a0cbc93149144566d
reidlo5135 <201840103@email.daelim.ac.kr>
2022-03-08 16:18:06 +0900

    3/8 reInitial Commit
    initial commit

---
+++ ICT2022-picTO/src/test/java/kr/co/picTO/Ict2022PicToApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.picTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ict2022PicToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
